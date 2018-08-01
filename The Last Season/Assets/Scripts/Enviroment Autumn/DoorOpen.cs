43f8501c6200c773e29f5a9d8d0bf82cd9080c2b
Mel-Tem <MeltemOezkul@gmail.com>
2018-08-01 17:08:21 +0200

    Structur Room

--- The Last Season/Assets/Scripts/Enviroment Autumn/DoorOpen.cs
+++ The Last Season/Assets/Scripts/Enviroment Autumn/DoorOpen.cs
@@ -1,36 +1,16 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class DoorOpen : MonoBehaviour
-{
-	public GameObject guiObject;
-	//public GameObject player;
+public class DoorOpen : MonoBehaviour {	
 	
 	public string levelToLoad;
-	
-	// Use this for initialization
-	void Start () {
-		guiObject.SetActive(false);
-	}
 
-	void OnTrigger(Collider other)
+	void OnTriggerStay(Collider other)
 	{
-		if (other.gameObject.tag == "Player")
+		if (other.tag == "Player")
 		{
-			guiObject.SetActive(true);
-			if (guiObject.activeInHierarchy == true && Input.GetButtonDown("Use"))
-			{
-				Application.LoadLevel(levelToLoad);
-
-			}
-			
+			Application.LoadLevel(levelToLoad);
+		
 		}
-	}
-
-	void OnTrigerExit()
-	{
-		guiObject.SetActive(false);
 	}
 }
