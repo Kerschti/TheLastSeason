a666d9409793b12270f9200c48f850cb91380fdf
Kerschti <gd_sp@hotmail.de>
2018-08-02 17:03:46 +0200

    Added Zombunny which follows player

---
+++ The Last Season/Assets/Scripts/Enemys/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+    private Transform player;
+    private NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(player.position);
+	}
+}
