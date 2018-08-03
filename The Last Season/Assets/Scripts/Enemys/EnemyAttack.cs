56ddb405bca441b598214cfa8114e25903161122
Kerschti <gd_sp@hotmail.de>
2018-08-03 11:47:43 +0200

    added EnemyAttack & possible Death of Player through PlayerHealth
    Health of Player is not yet shown & will die instantly when in range, since there is no pause between enemy attacks yet.

---
+++ The Last Season/Assets/Scripts/Enemys/EnemyAttack.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+
+    private GameObject player;
+    private bool playerInRange;
+    private PlayerHealth playerHealth;
+
+    public int atkDamage = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange)
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        if(playerHealth.curHealth > 0)
+        {
+            playerHealth.TakeDamage(atkDamage);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If the entering collider is the player...
+        if (other.gameObject == player)
+        {
+            // ... the player is in range.
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        // If the exiting collider is the player...
+        if (other.gameObject == player)
+        {
+            // ... the player is no longer in range.
+            playerInRange = false;
+        }
+    }
+
+}
---
+++ The Last Season/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int health = 100;
+    public int curHealth;
+    private bool damaged;
+    private bool dead = false;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        curHealth = health;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+
+        damaged = true;
+
+        curHealth -= amount;
+
+        if(curHealth <=0 && !dead)
+        {
+            Death();
+        }
+
+
+    }
+
+    void Death()
+    {
+        dead = true;
+
+        anim.SetTrigger("IsDead");
+    }
+}
