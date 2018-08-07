a7173a1c463e1e6921a9ef085840032681f717f8
Kerschti <gd_sp@hotmail.de>
2018-08-07 17:35:33 +0200

    some work @flyingRock

--- The Last Season/Assets/Scripts/Environment Winter/FlyingRock.cs
+++ The Last Season/Assets/Scripts/Environment Winter/FlyingRock.cs
@@ -2,32 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlyingRock : MonoBehaviour {
+public class FlyingRock : MonoBehaviour
+{
 
 
     //public GameObject rock;
     public GameObject player;
+    public Vector3 height;
+    public float speed;
 
     private Collider playerCollider;
+    private PlayerMovement playerMove;
 
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         playerCollider = player.GetComponent<Collider>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        playerMove = player.GetComponent<PlayerMovement>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other == playerCollider){
-            this.transform.Translate(Vector3.up);
+        if (other == playerCollider)
+        {
+            Debug.Log("entered");
+            StartCoroutine(FlyUp());
+
         }
     }
 
+    private IEnumerator FlyUp()
+    {
+        yield return new WaitForSeconds(1f);
+        transform.Translate(height * speed * Time.deltaTime, Space.World);
+        playerMove.enabled = false;
+        yield return new WaitForSeconds(1f);
+        playerMove.enabled = true;
+    }
 
 }
