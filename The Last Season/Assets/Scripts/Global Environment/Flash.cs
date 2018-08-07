242eabb2e836b0e6fd149c601b8230c8b935fb5c
Kerschti <gd_sp@hotmail.de>
2018-08-07 17:12:23 +0200

    some weather and overall atmosphere adjustments

---
+++ The Last Season/Assets/Scripts/Global Environment/Flash.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Flash : MonoBehaviour
+{
+
+
+    public float flashingTime = 0.5f;
+    private float waitingTime;
+    private Light lightning;
+
+    // Use this for initialization
+    void Start()
+    {
+        Random.InitState(DateTime.Now.Millisecond);
+        lightning = GetComponent<Light>();
+        lightning.intensity = 0;
+        waitingTime = Random.Range(0, 10);
+        StartCoroutine(StartFlash());
+
+    }
+
+    private IEnumerator StartFlash()
+    {
+
+        while (true)
+        {
+            lightning.intensity = 0;
+            yield return new WaitForSecondsRealtime(waitingTime);
+            waitingTime = Random.Range(0, 10);
+            lightning.intensity = 1;
+            yield return new WaitForSeconds(flashingTime);
+        }
+    }
+}
