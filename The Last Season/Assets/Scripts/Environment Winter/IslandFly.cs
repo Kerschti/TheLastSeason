11474ba16ab70126db18d7226cbb7c7c5eb2e0ce
Kerschti <gd_sp@hotmail.de>
2018-08-15 14:42:19 +0200

    Deleted Unused Stuff, added thirdIsland

---
+++ The Last Season/Assets/Scripts/Environment Winter/IslandFly.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandFly : MonoBehaviour {
+
+    public float speed;
+    public Transform ThirdIsland;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AnimateThirdIsland();
+
+	}
+
+    void AnimateThirdIsland()
+    {
+        ThirdIsland.RotateAround(transform.position, new Vector3(1, 0, 0), speed * Time.deltaTime);
+    }
+}
