cc247f1222a4161370691399eb557577a7d813e1
Mel-Tem <MeltemOezkul@gmail.com>
2018-08-20 16:54:15 +0200

    Old

--- The Last Season/Assets/Scripts/Enviroment Autumn/RoomTestScript.cs
+++ The Last Season/Assets/Scripts/Enviroment Autumn/RoomTestScript.cs
@@ -1,20 +1,61 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using Unity.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms;
 
 public class RoomTestScript : MonoBehaviour
 {
-	//private GameObject wall;
-	//public int left, right, top, bot;
+	//public RoomTestScript(){}
 
-	public RoomTestScript()
+	//private float breite = 0.0f;
+	private float a, b; // xy
+	private float wallA, wallB;
+	private  Vector3 start = new Vector3(0, 0, 0);
+	public GameObject WallPrefab;
+	
+	public void Zufall()
 	{
+		for (int i = 1; i <= 10; i++)
+		{
+			a = Random.Range(5.0f, 12.0f);
+		}
 
+		for (int i = 1; i <= 10; i++)
+		{
+			b = Random.Range(5.0f, 12.0f);
+		}
 	}
 
-	// Use this for initialization
+	// Instanz von Prefabs Wall
+	public void createWall()
+	{
+		// Hier Rechteck a*b und dann Spiegelen die -a*-b
+	
+	}
+
+
 	void Start()
 	{
+		for (int y = 1; y <= 5; y++)
+		{
+			Instantiate(WallPrefab, new Vector3(-6.6f, y*0.75f - 0.25f, 0), Quaternion.identity);
+		}
+
+		// rotieren Urspung 
+		// Abstand von Urspung und Wand
+		// Oder a*b dass Spiegeln   --> Wand Prefab erstellen und hochziehen,
+/*
+		GameObject cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube1.transform.localScale = new Vector3(links, breite, hoehe); //xyz
+
+		GameObject cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube2.transform.localScale = new Vector3(breite, rechts, hoehe);
+
+*/
+/*
+		//Vector3 links = new Vector3(Random.Range(5.0f, 12.0f), 0, Random.Range(5.0f, 12.0f));
+		//Vector3 rechts = new Vector3(Random.Range(5.0f, 12.0f), 0, Random.Range(5.0f, 12.0f));	
 
 		// Wand links
 		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
@@ -26,25 +67,26 @@ public class RoomTestScript : MonoBehaviour
 		GameObject cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		cube2.transform.localScale = new Vector3(0.5F, 2.5F, 10);
 		cube2.transform.position = new Vector3(7.44F, 1.6F, 3);
-		
+
 		// Wand Eingang
 		GameObject cube3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		cube3.transform.position = new Vector3(5, 1.16F, 8);
 		//cube.transform.localRotation = new Vector3(0, 90F, 0);
 		cube3.transform.localScale = new Vector3(0.5F, 2.5F, 10);
-		cube3.transform.Rotate( new Vector3(0, 90F, 0));
-/**		
+		cube3.transform.Rotate(new Vector3(0, 90F, 0));
+
 		// Wand Ausgang
 		GameObject cube4 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		cube.transform.position = new Vector3(5,  1.16F, -1.78F);
+		cube.transform.position = new Vector3(5, 1.16F, -1.78F);
 		cube.transform.rotation = new Vector3(0, 90F, 0);
 		cube.transform.localScale = new Vector3(0.5F, 2.5F, 10);
-	}*/
+		*/
 
-		// Update is called once per frame
-		/*void Update()
+		/*// Update is called once per frame
+		void Update()
 		{
-		
-		}*/
+
+		}
+*/
 	}
 }
--- The Last Season/Assets/bla.cs
+++ The Last Season/Assets/bla.cs
@@ -1,45 +1,4 @@
-﻿
-using System.Collections; 
-using UnityEngine;
-/**
-	//Einen Raum generieren
-
-public class RoomGenerator : MonoBehaviour
-{
-	// Vier GameObjekte 2*a 2*b
-	
-	Vector2 roomSize = new Vector2(4, 4);
-
-	private int Room;
-	private int rooms;
-
-	int wallSizeX, wallSizeY, numberOfRooms = 1;
-
-	public GameObject roomWithObject;
-	
-	// Use this for initialization
-	void Start () {
-		if (numberOfRooms >= (roomSize.x * 2) * (roomSize.y * 2))	
-		{
-			numberOfRooms = Mathf.RoundToInt((roomSize.x * 2) * (roomSize.y * 2));
-		}
-
-		wallSizeX = Mathf.RoundToInt(roomSize.x);
-		wallSizeY = Mathf.RoundToInt(roomSize.y);
-
-		CreateRooms();
-		//SetRoomDoors();
-	}
-	
-	void CreateRooms ()
-	{
-		rooms = new Room[wallSizeX * 2, wallSizeY * 2];
-	}
-}
-*/
-
-/*
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -141,5 +100,3 @@ public class kochband : MonoBehaviour
         zaehler += 4;
     }
 }
-
-*/
