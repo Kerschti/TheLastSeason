f708f8bf582f442918bcb6124b22935bc2d39b8f
Kerschti <gd_sp@hotmail.de>
2018-08-21 11:24:00 +0200

    Some script adjustments, fixed line endings error

--- The Last Season/Assets/Scripts/Player/PlayerHealth.cs
+++ The Last Season/Assets/Scripts/Player/PlayerHealth.cs
@@ -12,6 +12,11 @@ public class PlayerHealth : MonoBehaviour
     private bool dead = false;
     private Animator anim;
     private PlayerMovement playerMove;
+    private float lastY;                        // Float that stores last Position of player when falling
+    private float lastYTravelDistance;          // Float that stores the calculated distance traveled beween last frame of falling and now
+    private float fallHeight = 0;               // Float to determine if falling.
+    protected float deathHeight = 8;            // Float of max Height you can fall before dieing.
+    private bool wasFalling = false;            // Determine if player fell in last frame.
 
     // Use this for initialization
     void Start()
@@ -21,11 +26,13 @@ public class PlayerHealth : MonoBehaviour
         playerMove = GetComponent<PlayerMovement>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
+    void FixedUpdate()
+    {
+        CheckForFall();
     }
+
+
 
     public void TakeDamage(int amount)
     {
@@ -36,7 +43,7 @@ public class PlayerHealth : MonoBehaviour
 
         Debug.Log("Health of Player: " + curHealth);
 
-        if(curHealth <=0 && !dead)
+        if (curHealth <= 0 && !dead)
         {
             Death();
         }
@@ -53,11 +60,49 @@ public class PlayerHealth : MonoBehaviour
 
     }
 
+    void CheckForFall()
+    {
+
+        if (!playerMove.IsGrounded())
+        {
+
+            // Calculate the distance between players current height and the height he was in the last frame.
+            lastYTravelDistance = transform.position.y - lastY;
+
+            // If difference is negative, store descending value. 
+            fallHeight += lastYTravelDistance < 0 ? lastYTravelDistance : 0;
+
+            // Cache players current Y position for comparison in the next frame.
+            lastY = transform.position.y;
+
+            //store that a fall happened
+            //wasFalling = true;
+
+            //Debug.Log("Travel Distance: "+ lastYTravelDistance);
+        }
+        else if (playerMove.IsGrounded()/* && wasFalling*/)
+        {
+
+            // Check to see if player passed the allowed falling distance and kill the player if necessary.
+            if (fallHeight <= -deathHeight)
+                TakeDamage(100);
+
+            //reset fall height since we landed (doesn't matter if we're dead or alive)
+            fallHeight = 0;
+
+            //rest lastY
+            //lastY = transform.position.y;
+            //checked if player fell to death now prevent repeating
+            //wasFalling = false;
+        }
+
+
+    }
+
     private IEnumerator SpawnAfterSec()
     {
         yield return new WaitForSeconds(5f);
         transform.position = spawnPoint.position;
-
         curHealth = health;
         playerMove.enabled = true;
         dead = false;
--- The Last Season/Assets/Scripts/Player/PlayerMovement.cs
+++ The Last Season/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [System.Serializable]
+    public class SpeedSetting
+    {
+        public float jumpForce = 10f;                // Upward force for jumping.
+        public float Speed = 3f;                    // moving speed.
+        public float runSpeed = 6f;                 // run speed.
+        public float rotationSpeed = 75f;           // rotation speed.
+        public float downwardAccel = 0.6f;          // Acceleration at which player falls downwards. 
+    }
+
+    [System.Serializable]
+    public class TimeSetting
+    {
+        public float smoothTime = 0.1f;             // Smoothtime for smoothing playermovement.
+        public float timeUntilRuns = 20;            // Timer until the player runs.
+    }
+
+
     //private Vector3 movement;                   // The vector to store the direction of the player's movement.
     private Animator anim;                      // Reference to the animator component.
     private Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
@@ -11,11 +30,6 @@ public class PlayerMovement : MonoBehaviour
     private int waterMask;                      // A layer mask for the Water layer.
     private Vector2 rotation;                   // Vector to store the direction in wich the player should turn.
     private CapsuleCollider col;                // Reference to the Players CapsuleCollider.
-    private float lastY;                        // Float that stores last Position of player when falling
-    private float lastYTravelDistance;          // Float that stores the calculated distance traveled beween last frame of falling and now
-    private float fallHeight = 0;               // Float to determine if falling.
-    private float deathHeight = 8;              // Float of max Height you can fall before dieing.
-    private bool wasFalling = false;            // Determine if player fell in last frame.
     private float h;                            // Stores horizontal input
     private float v;                            // Stores vertical input.
     private float initTime;                     // Stores inital value for timeUntilRuns.
@@ -23,20 +37,11 @@ public class PlayerMovement : MonoBehaviour
     private float SpeedVelocity;                // Obligatory Velocity value (0) for speedcalculation.
     private float curSpeed;                     // Stores current Speed of player.
     private float verticalVelocity;             // Setting of upward velocity.
-
-    public float smoothTime = 0.1f;             // Smoothtime for smoothing playermovement.
-    public float timeUntilRuns = 20;            // Timer until the player runs.
-    public float jumpForce = 2f;                // Upward force for jumping.
-    public float Speed = 7f;                    // moving speed.
-    public float runSpeed = 5f;                 // run speed.
-    public float rotationSpeed = 75f;           // rotation speed.
-    public float downwardAccel = 0.3f;          // Acceleration at which player falls downwards.
     Transform cameraTrans;                      // camera transform.
     Vector2 dir;                                // Vector2 for calculating movement & rotation of player.
 
-
-
-
+    public SpeedSetting speedSetting = new SpeedSetting();
+    public TimeSetting timeSetting = new TimeSetting();
 
     void Awake()
     {
@@ -48,12 +53,13 @@ public class PlayerMovement : MonoBehaviour
         playerRigidbody = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         cameraTrans = Camera.main.transform;
-        initTime = timeUntilRuns;
-        initSpeed = Speed;
+        initTime = timeSetting.timeUntilRuns;
+        initSpeed = speedSetting.Speed;
 
     }
 
-    void GetInput(){
+    void GetInput()
+    {
         // Store the input axes.
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
@@ -73,14 +79,11 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
 
-        // Check if player is falling to death.
-        CheckForFall();
-
-
         // Move the player around the scene.
         //TODO: prevent moving the player while he's attacking 
         Move();
 
+        //Jump if desired.
         Jump();
 
         // Animate the player.
@@ -96,19 +99,19 @@ public class PlayerMovement : MonoBehaviour
         // Check if the Player is walking and isn't swimming
         bool IsWalking = h != 0f || v != 0f && !IsInWater();
 
-        if(IsWalking && timeUntilRuns >= 0)
+        if (IsWalking && timeSetting.timeUntilRuns >= 0)
         {
             // Subtract actual Time from timer, if Timer <= 0, trigger running state & stop checking, unless player stopped moving. 
-            timeUntilRuns -= Time.deltaTime;
+            timeSetting.timeUntilRuns -= Time.deltaTime;
 
         }
-        else if(!IsWalking)
+        else if (!IsWalking)
         {
-            timeUntilRuns = initTime;
+            timeSetting.timeUntilRuns = initTime;
         }
     }
 
-    private bool IsGrounded()
+    public bool IsGrounded()
     {
         return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x,
                                                                    col.bounds.min.y,
@@ -128,17 +131,16 @@ public class PlayerMovement : MonoBehaviour
     {
 
         // Set speed based on direction & run or walkspeed, based on how long he moved.
-        Speed = (timeUntilRuns <= 0 ? runSpeed : initSpeed) * dir.magnitude;
+        speedSetting.Speed = (timeSetting.timeUntilRuns <= 0 ? speedSetting.runSpeed : initSpeed) * dir.magnitude;
 
         // Smoothen speedgain over time.
-        curSpeed = Mathf.SmoothDamp(curSpeed, Speed, ref SpeedVelocity, smoothTime);
+        curSpeed = Mathf.SmoothDamp(curSpeed, speedSetting.Speed, ref SpeedVelocity, timeSetting.smoothTime);
 
         // Translate Player forward by speed. 
         transform.Translate(transform.forward * curSpeed * Time.deltaTime, Space.World);
         //playerRigidbody.AddForce(transform.forward * movement * Time.deltaTime, ForceMode.Impulse);
         //Debug.Log(movement);
         //playerRigidbody.velocity = transform.forward * movement * Time.deltaTime;
-
 
     }
 
@@ -149,12 +151,12 @@ public class PlayerMovement : MonoBehaviour
         rotation.Set(h, v);
         dir = rotation.normalized;
 
-        if(dir != Vector2.zero)
+        if (dir != Vector2.zero)
         {
             // Calculate players rotation by radians. Add CamRotation, so player is going forward according to camera rotation.
             float targetRot = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg + cameraTrans.eulerAngles.y;
             // Set transforms rotation smoothly.
-            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRot, ref rotationSpeed, 0.15f);
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRot, ref speedSetting.rotationSpeed, 0.15f);
         }
 
     }
@@ -162,68 +164,31 @@ public class PlayerMovement : MonoBehaviour
     void Jump()
     {
 
-        if(IsGrounded())
+        if (IsGrounded())
         {
+            
             verticalVelocity = 0;
-            if(Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                verticalVelocity = jumpForce;
-            }                                    
+                
+                verticalVelocity = speedSetting.jumpForce;
+            }
         }
         else
         {
-            verticalVelocity -= downwardAccel;
+            verticalVelocity -= speedSetting.downwardAccel;
         }
 
         Vector3 jumpVector = new Vector3(0, verticalVelocity, 0);
-        //Debug.Log(jumpVector);
+
         playerRigidbody.velocity = transform.TransformDirection(jumpVector);
     }
 
 
-    void CheckForFall()
-    {
-        
-        if (!IsGrounded())
-        {
-            
-            // Calculate the distance between players current height and the height he was in the last frame.
-            lastYTravelDistance = transform.position.y - lastY;
-
-            // If difference is negative, store descending value. 
-            fallHeight += lastYTravelDistance < 0 ? lastYTravelDistance : 0;
-
-            // Cache players current Y position for comparison in the next frame.
-            lastY = transform.position.y;
-
-            //store that a fall happened
-            //wasFalling = true;
-
-            //Debug.Log("Travel Distance: "+ lastYTravelDistance);
-        }
-        else if(IsGrounded()/* && wasFalling*/)
-        {
-            
-            // Check to see if player passed the allowed falling distance and kill the player if necessary.
-            if (fallHeight <= -deathHeight)
-                GetComponent<PlayerHealth>().TakeDamage(100);
-            
-            //reset fall height since we landed (doesn't matter if we're dead or alive)
-            fallHeight = 0;
-
-            //rest lastY
-            //lastY = transform.position.y;
-            //checked if player fell to death now prevent repeating
-            //wasFalling = false;
-        }
-
-
-    }
-
     void Animating()
     {
         bool IsWalking = h != 0f || v != 0f && !IsInWater();
-        float running = timeUntilRuns <= 0 ? 1f : 0.5f;
+        float running = timeSetting.timeUntilRuns <= 0 ? 1f : 0.5f;
 
         anim.SetBool("IsInWater", IsInWater());
 
@@ -241,11 +206,11 @@ public class PlayerMovement : MonoBehaviour
 
         if (IsWalking)
         {
-            
+
             anim.SetFloat("SpeedPerc", running);
 
         }
-      
+
         else if (!IsWalking)
         {
             anim.SetFloat("SpeedPerc", 0);
