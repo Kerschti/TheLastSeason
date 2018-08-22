d6e536cebd0ca2c390b3cb5987d5243915373552
Tanschi <35659698+Tanschi@users.noreply.github.com>
2018-08-22 14:05:18 +0200

    debugging

--- The Last Season/Assets/Scripts/Enemys/HellephantShoot.cs
+++ The Last Season/Assets/Scripts/Enemys/HellephantShoot.cs
@@ -9,31 +9,57 @@ public class HellephantShoot : MonoBehaviour {
 	public Transform trunk;
 	public GameObject bullet;
 
-	void Awake(){
-		player = GameObject.FindWithTag ("Player").transform;
-	}
+    private bool playerInRange;
+    private PlayerHealth playerHealth;
+    public int atkDamage = 5;
+    private float timer;
+    public float timeBetweenAttacks = 0.5f;
 
-	void Update(){
+    void Awake(){
+		player = GameObject.FindWithTag ("Player").transform;
+        playerHealth = player.GetComponent<PlayerHealth>();
+        
+    }
+
+    void Update(){
 		transform.LookAt (player);
-	}
+
+        timer += Time.deltaTime;
+
+        if (timer >= timeBetweenAttacks && playerInRange)
+        {
+            Attack();
+        }
+    }
 
     //if player in trigger start shooting
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "Player") {
 			StartCoroutine ("Shooting");
-		}
+            playerInRange = true;
+        }
+
+  
 	}
 
     //If player not in trigger stop shooting
 	void OnTriggerExit(Collider other){
 		if (other.gameObject.tag == "Player") {
-			StopCoroutine ("Shooting");
+            playerInRange = false;
+            StopCoroutine ("Shooting");
 		}
 	}
 
+    void Attack()
+    {
+        if (playerHealth.curHealth > 0)
+        {
+            playerHealth.TakeDamage(atkDamage);
+        }
+    }
 
     //Create new bullet every one second
-	IEnumerator Shooting(){
+    IEnumerator Shooting(){
         
 		while (true) {
 			Instantiate (bullet, trunk.position, trunk.rotation);
@@ -42,9 +68,9 @@ public class HellephantShoot : MonoBehaviour {
 
 
 	}
-		
 
-	}
+
+}
 
 
 
