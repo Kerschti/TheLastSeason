ba2a5650ce69948480d703e2fe447e36972ac11d
Mel-Tem <MeltemOezkul@gmail.com>
2018-09-05 21:06:23 +0300

    Door

--- The Last Season/Assets/Scripts/Enviroment Autumn/DoorOpen.cs
+++ The Last Season/Assets/Scripts/Enviroment Autumn/DoorOpen.cs
@@ -1,30 +1,17 @@
-﻿/*using System.Collections;
+﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class DoorOpen : MonoBehaviour {	
 	
-	public string levelToLoad;
+	//public string levelToLoad;
 
 	void OnTriggerStay(Collider other)
 	{
 		if (other.tag == "Player")
 		{
-			Application.LoadLevel(levelToLoad);
-		
+			
+			SceneManager.LoadScene ("Room", LoadSceneMode.Single);
+
 		}
 	}
 }
-*/
-
-/*
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class DoorOpen : MonoBehaviour {
-	void Start () {
-		// Only specifying the sceneName or sceneBuildIndex will load the scene with the Single mode
-		SceneManager.LoadScene ("Room", LoadSceneMode.Additive);
-	}
-}
-
-*/
