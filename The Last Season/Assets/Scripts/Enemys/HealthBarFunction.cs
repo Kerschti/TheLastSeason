ccdc7925da2b68081b94a970385612c495532582
Mel-Tem <MeltemOezkul@gmail.com>
2018-09-06 14:03:37 +0300

    HeathBar

---
+++ The Last Season/Assets/Scripts/Enemys/HealthBarFunction.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class HealthBarFunction : MonoBehaviour
+{
+
+	private Slider healthBar;
+	private int currentHP = 100;
+
+	//private HealthBarFunction healthBar;
+	
+	private void Awake()
+	{
+		//healthBar = GameObject.Find("HealthBar").GetComponent<HealthBarFunction>();
+		healthBar = GetComponent<Slider>();
+	}
+	
+	void Update ()
+	{
+		healthBar.value = currentHP;
+	}
+
+	public void changeHP(int dHP)
+	{
+		currentHP += dHP;
+	}
+}
---
+++ The Last Season/Assets/Scripts/Enemys/HealthBarTester.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarTester : MonoBehaviour
+{
+
+	private HealthBarFunction healthBar;
+
+	void Awake()
+	{
+		healthBar = GameObject.Find("Health Bar").GetComponent<HealthBarFunction>();
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			healthBar.changeHP(1);
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			healthBar.changeHP(-1);
+		}
+	}
+}
