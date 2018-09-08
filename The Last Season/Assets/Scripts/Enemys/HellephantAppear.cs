efbf7fa03a5f3ea0aab3e69cde94ddc9a64040c1
Tanschi <35659698+Tanschi@users.noreply.github.com>
2018-09-08 13:49:54 +0200

    Hellephant Fixing

--- The Last Season/Assets/Scripts/Enemys/HellephantAppear.cs
+++ The Last Season/Assets/Scripts/Enemys/HellephantAppear.cs
@@ -7,15 +7,21 @@ public class HellephantAppear : MonoBehaviour {
 
     private float timer;
     GameObject killerhellephant;
-    int x = 25;
-    int y = 10;
+
+    int x = 52;
+    int y = 3;
     int z = 55;
+
+    int x2 = 14;
+    int y2 = 3;
+    int z2 = 17;
+
     Vector3 position;
     private int c = 0;
+    private int c2 = 0;
 
 	void Awake () {
         killerhellephant = GameObject.FindGameObjectWithTag("KillerHellephant");
-        
 	}
 
     void OnTriggerEnter(Collider other)
@@ -24,15 +30,27 @@ public class HellephantAppear : MonoBehaviour {
         {
             timer += Time.deltaTime;
 
-            while (timer == timer % 5f && c < 6)
+            while (timer == timer % 5f && c < 4)
             {
                 position = new Vector3(x, y, z);
-                Debug.Log("Hellephant Appear");
                 Instantiate(killerhellephant, position, Quaternion.identity);
                 c++;
                 x = x + 25;
-                Debug.Log("Jetzzt kommt ein neuer Elefant");
+                Debug.Log("Jetzt kommt ein neuer Elefant");
             }
+
+            while (timer == timer % 10f && c2 < 4)
+            {
+                position = new Vector3(x2, y2, z2);
+                
+                Instantiate(killerhellephant, position, Quaternion.identity);
+                c2++;
+                x2 = x2 + 25;
+                y2 = 3;
+                z2 = 17;
+                Debug.Log("Jetzt kommt ein neuer Elefant 22");
+            }
+
         }
 
         
