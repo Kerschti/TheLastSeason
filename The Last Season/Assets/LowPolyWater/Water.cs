9ce197dd3dbf9af7acdc29c5e9f021b50600e15f
Tanschi <35659698+Tanschi@users.noreply.github.com>
2018-09-15 21:27:24 +0200

    edit water scripting

--- The Last Season/Assets/LowPolyWater/Water.cs
+++ The Last Season/Assets/LowPolyWater/Water.cs
@@ -3,20 +3,23 @@ using System.Collections;
 
 public class Water : MonoBehaviour 
 {
-    public float scale = 7.0f;
-    public float heightScale = 1.0f;
-    private Vector2 v2SampleStart = new Vector2(0f, 0f);
+    //wave scale
+    public float scale = 1.0f;
 
 	// Update is called once per frame
 	void Update () 
     {
         MeshFilter mf = GetComponent<MeshFilter>();
+
+        //create new mesh with x,y,z with Vector3D Array
         Vector3[] vertices = mf.mesh.vertices;
 
         for (int i = 0; i < vertices.Length; i++)
         {
-            vertices[i].y = heightScale * Mathf.PerlinNoise(Time.time + (vertices[i].x * scale), Time.time + (vertices[i].z * scale));
+            //new data, calculation was inspired by https://www.youtube.com/watch?v=3MoHJtBnn2U
+            vertices[i].y = scale * Mathf.PerlinNoise(Time.time + (vertices[i].x * scale), Time.time + (vertices[i].z * scale));
         }
+        //new potion for vertices 
         mf.mesh.vertices = vertices;
 	}
 }
